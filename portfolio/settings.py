07180e0de62cbb748eccd0f3afe695aa3e98b673
ndongchrist <christianhonore2003@gmail.com>
2024-04-26 12:32:41 +0100

    Feat: Changed the UI

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -125,10 +125,10 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media_root/')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
-
+STATIC_ROOT =  os.path.join(BASE_DIR, 'static_root/')
+    
 STATICFILES_DIRS = [
-    BASE_DIR / 'static',
+    os.path.join(BASE_DIR / 'static'),
 ]
 
 # Default primary key field type
